32bdce21608c1ba5ed3967fa6df80db7378924f7
Aleksandar Bogatinov <aleksandar.bogatinov@haselt.com>
2021-12-11 23:31:20 +0100

    Add Day03

---
+++ AdventOfCode2021/Benchmarks/Day03Benchmarks.cs
@@ -0,0 +1,9 @@
+﻿using AdventOfCode2021.Advents;
+
+namespace AdventOfCode2021.Benchmarks
+{
+    public class Day03Benchmarks : DayAdventBenchmark<int>
+    {
+        public override void Setup() => Day = new Day03("Puzzles/day03.txt");
+    }
+}
---
+++ AdventOfCode2021/Tests/Day03Tests.cs
@@ -0,0 +1,51 @@
+﻿using AdventOfCode2021.Advents;
+using Xunit;
+
+namespace AdventOfCode2021.Tests
+{
+    public class Day03Tests : DayAdventFixture<int>
+    {
+        public Day03Tests() : base(new Day03("Puzzles/day03.txt"))
+        {
+        }
+
+        [Fact(DisplayName = "Day03 Solve01")]
+        public override void Solve1()
+        {
+            var result = Day.Solve1();
+
+            Assert.Equal(1997414, result);
+        }
+
+        [Fact(DisplayName = "Day03 Solve02")]
+        public override void Solve2()
+        {
+            var result = Day.Solve2();
+
+            Assert.Equal(1032597, result);
+        }
+    }
+
+    public class Day03TrainTests : DayAdventFixture<int>
+    {
+        public Day03TrainTests() : base(new Day03("Puzzles/day03_train.txt"))
+        {
+        }
+
+        [Fact(DisplayName = "Day03 Train Solve01")]
+        public override void Solve1()
+        {
+            var result = Day.Solve1();
+
+            Assert.Equal(198, result);
+        }
+
+        [Fact(DisplayName = "Day03 Train Solve02")]
+        public override void Solve2()
+        {
+            var result = Day.Solve2();
+
+            Assert.Equal(230, result);
+        }
+    }
+}
