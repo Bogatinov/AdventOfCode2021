5be427f4c7f6636a5ff1cd633fb3b768b173c835
Aleksandar Bogatinov <aleksandar.bogatinov@haselt.com>
2021-12-12 00:01:30 +0100

    Add Day06

---
+++ AdventOfCode2021/Advents/Day06.cs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCode2021.Advents
+{
+    public class Day06 : DayAdvent<long>
+    {
+        private readonly long[] lives;
+
+        public Day06(string filePath) : base(filePath)
+        {
+            lives = new long[9] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            foreach (var line in Lines[0].Split(","))
+            {
+                lives[int.Parse(line) + 1]++;
+            }
+        }
+
+        public override long Solve1() => Solve(80);
+
+        public override long Solve2() => Solve(256);
+
+        private long Solve(int days)
+        {
+            short maxLife = 9;
+            for (int day = 0; day <= days; day++)
+            {
+                lives[(day + 7) % maxLife] += lives[day % maxLife];
+            }
+
+            long result = 0;
+            for (int i = 0; i < lives.Length; i++)
+            {
+                result += lives[i];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ AdventOfCode2021/Tests/Day06Tests.cs
@@ -0,0 +1,51 @@
+﻿using AdventOfCode2021.Advents;
+using Xunit;
+
+namespace AdventOfCode2021.Tests
+{
+    public class Day06Tests : DayAdventFixture<long>
+    {
+        public Day06Tests() : base(new Day06("Puzzles/day06.txt"))
+        {
+        }
+
+        [Fact(DisplayName = "Day06 Solve01")]
+        public override void Solve1()
+        {
+            var result = Day.Solve1();
+
+            Assert.Equal(363101, result);
+        }
+
+        [Fact(DisplayName = "Day06 Solve02")]
+        public override void Solve2()
+        {
+            var result = Day.Solve2();
+
+            Assert.Equal(1644286074024, result);
+        }
+    }
+
+    public class Day06TrainTests : DayAdventFixture<long>
+    {
+        public Day06TrainTests() : base(new Day06("Puzzles/day06_train.txt"))
+        {
+        }
+
+        [Fact(DisplayName = "Day06 Train Solve01")]
+        public override void Solve1()
+        {
+            var result = Day.Solve1();
+
+            Assert.Equal(5934, result);
+        }
+
+        [Fact(DisplayName = "Day06 Train Solve02")]
+        public override void Solve2()
+        {
+            var result = Day.Solve2();
+
+            Assert.Equal(26984457539, result);
+        }
+    }
+}
